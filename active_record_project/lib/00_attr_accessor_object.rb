036d3466e1d49ab7ebceed70203e2d8ed2aeb0a2
aseemar <ajseemar@gmail.com>
2019-03-29 16:03:13 -0700

    Finish up to initialize method for SQLObject

--- active_record_project/lib/00_attr_accessor_object.rb
+++ active_record_project/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |attr|
+      define_method(attr) { instance_variable_get("@#{attr}") }
+
+      define_method("#{attr}=") do |arg|
+        instance_variable_set("@#{attr}", arg)
+      end
+
+    end
   end
 end
--- active_record_project/lib/01_sql_object.rb
+++ active_record_project/lib/01_sql_object.rb
@@ -6,21 +6,38 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    @cols ||= DBConnection.execute2(<<-SQL)
+      SELECT *
+      FROM #{self.table_name}
+    SQL
+    .first.map { |el| el.to_sym }
   end
 
   def self.finalize!
+    columns.each do |attr|
+      define_method(attr) { attributes[attr] }
+      define_method("#{attr}=") { |val| attributes[attr] = val}
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    self.to_s.downcase + 's'  # name  + 's'
   end
 
   def self.all
     # ...
+    arg1 = table_name
+    puts arg1
+    DBConnection.execute(<<-SQL, arg1)
+      SELECT *
+      FROM ?
+    SQL
   end
 
   def self.parse_all(results)
@@ -33,10 +50,16 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    params.each do |k, v|
+      raise "unknown attribute '#{k}'" unless self.class.columns.include?(k)
+      send("#{k}=", v)
+    end
+    
   end
 
   def attributes
     # ...
+    @attributes ||= Hash.new
   end
 
   def attribute_values
